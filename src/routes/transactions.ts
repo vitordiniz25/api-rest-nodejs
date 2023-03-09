20218d95ea163e86f618a957fd42a2f7bbcd14d6
vitordiniz25 <vitordiniz25@gmail.com>
2023-03-09 20:55:38 -0300

    get transactions routes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,30 @@ import crypto from 'node:crypto'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return {
+      transactions,
+    }
+  })
+
+  // http://localhost:3333/transactions/jasbdasjbd-asjkdbasjkd-jkasbdjkas
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return {
+      transaction,
+    }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
